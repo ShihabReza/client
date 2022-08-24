088ddf424929bc42096e309db852f69d5e68d0b8
ShihabReza <shihabreza457@gmail.com>
2022-08-25 00:13:11 +0600

    first commit

---
+++ src/component/MainCard/MainCard.js
@@ -0,0 +1,79 @@
+import React from 'react';
+
+const MainCard = () => {
+  return (
+      <div class="card w- bg-base-100 shadow-xl mx-auto">
+            <div class="card-body ">
+              <div className='flex'>
+              <div >
+                  <div className='mb-2'>
+                    <h1 className='font-bold text-2xl'>gmuhit_five</h1>
+                  </div>
+                  <div class="avatar">
+                <div class="w-12 rounded-full">
+                  <img src="https://placeimg.com/192/192/people" />
+                </div>
+                
+              </div>
+              <p className='mb-2'>Member</p>
+               <div className='flex '>
+               <div>
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M8.625 9.75a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 01.778-.332 48.294 48.294 0 005.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
+            </svg>
+            
+                </div>
+                <div className='ml-1'>
+                  <h1>22</h1>
+                </div>
+                <div className='ml-2'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}stroke="currentColor" className="w-6 h-6">
+                     <path strokeLinecap="round" strokeLinejoin="round" d="M14.563 9.75a12.014 12.014 0 00-3.427 5.136L9 12.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+
+            
+                </div>
+                 
+                <div className='ml-1'>
+                  <h1>22</h1>
+                </div>
+                
+               
+              </div>
+              
+               </div>
+              <div className='ml-12'>
+                <div>
+                  <h1>Posted July 24</h1>
+                </div>
+                <div className='mb-2'>
+                  <h3>1. Many seller want to rank his or her gig by getting a order from buyer and want to get good review from buyer.</h3>
+                </div>
+                <div className='5'>
+                  <h3>2. Buyer have lot of option to do their order from seller.</h3>
+                </div>
+                
+              </div>
+              
+              </div>
+              <div class="card-actions justify-end">
+                <svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-red-600">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+</svg>
+<h1>22</h1>
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M15.182 16.318A4.486 4.486 0 0012.016 15a4.486 4.486 0 00-3.198 1.318M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z" />
+</svg>
+<h1>22</h1>
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M8.288 15.038a5.25 5.25 0 017.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 011.06 0z" />
+</svg>
+
+<h1>22</h1>
+    </div>
+           </div>
+      </div>
+  );
+};
+
+export default MainCard;
